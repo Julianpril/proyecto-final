9b16f551e1706f0524ba4253e972be0580ae72d5
karinamogollon <wkmogollon@uniboyaca.edu.co>
2026-05-12 17:10:12 -0500

    Comentarios

--- client/js/config.js
+++ client/js/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
     // MODO LOCAL (para desarrollo con servicios en localhost)
-    AUTH_API_URL: 'http://localhost:4000',
-    COORDINATOR_WS_URL: 'ws://localhost:5000',
+      AUTH_API_URL: 'https://breadless-keren-topfull.ngrok-free.dev',
+    COORDINATOR_WS_URL: 'wss://brock-prenegligent-judson.ngrok-free.dev',
 
     // MODO NGROK (sustentación) – descomenta cuando uses ngrok
     // AUTH_API_URL: 'https://breadless-keren-topfull.ngrok-free.dev',
